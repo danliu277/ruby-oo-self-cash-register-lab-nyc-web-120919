eb49cf97fa0a7cc5557923ce68e361e4e12fcc1a
Daniel Liu <danliu277@gmail.com>
2019-12-10 19:09:42 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -14,7 +14,7 @@ class CashRegister
     if discount == 0
       puts "There is no discount to apply"
     else
-      @total *= (1 + @discount)
+      @total *= (1 + @discount * .01)
     end
   end
 end
