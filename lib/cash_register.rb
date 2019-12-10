beb24e5780d2f7bc4493496e014de07ebd47399a
Daniel Liu <danliu277@gmail.com>
2019-12-10 19:13:38 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -14,7 +14,7 @@ class CashRegister
     if discount == 0
       puts "There is no discount to apply"
     else
-      @total *= (1 - @discount * 0.01)
+      @total = trim(@total * (1 - @discount * 0.01))
       "After the discount, the total comes to $#{@total}."
     end
   end
